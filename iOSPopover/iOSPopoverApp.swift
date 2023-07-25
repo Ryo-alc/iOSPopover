a723f82374d22269e3bbf9671d55b43d8156b413
Ryo-alc <138414112+Ryo-alc@users.noreply.github.com>
2023-07-25 13:51:23 +0900

    Initial Commit

---
+++ iOSPopover/iOSPopoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSPopoverApp.swift
+//  iOSPopover
+//
+//  Created by 岩永 涼 on 2023/07/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSPopoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
